5f6d1485e8394fba62d12842a40e18362aeccc0e
Kevin Smith <kevin.smith@jbhunt.com>
2020-08-02 21:17:23 -0500

    about to eject

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Lectorem from 'react-lectorem';
-import storyData from './frere.json';
+import storyData from './frere.data';
 import './App.css';
 
-function App() {
+require('react-dom');
+window.React2 = require('react');
+console.log('asdf react is equal', window.React1 === window.React2);
+
+const App = () => {
+  const [shouldShowTranslation, setShouldShowTranslation] = useState(true);
+  const [isTranslationBelow, setIsTranslationBelow] = useState(true);
+  const [isBySentence, setIsBySentence] = useState(false);
+
+  const toggleValue = (flag, setFlag) =>  setFlag(!flag);
+
+  console.log('asdf1', shouldShowTranslation);
   return (
-    <div className="App">
-      <Lectorem data={ storyData } />
+    <div className='App'>
+      <div onChange={ () => toggleValue(shouldShowTranslation, setShouldShowTranslation)}>
+        <input type='checkbox' value name='config' checked={ shouldShowTranslation }/>Translation
+      </div>
+
+      <div onChange={ () => toggleValue(isTranslationBelow, setIsTranslationBelow)}>
+        <input type='checkbox' value name='config' disabled={ !shouldShowTranslation } checked={ isTranslationBelow }/>Below
+      </div>
+
+      <div onChange={ () => toggleValue(isBySentence, setIsBySentence)}>
+        <input type='checkbox' value name='config' disabled={ !shouldShowTranslation } checked={ isBySentence }/>Sentence
+      </div>
+
+      <Lectorem
+        configuration={ { shouldShowTranslation, isTranslationBelow, isBySentence } }
+        data={ storyData }
+      />
     </div>
   );
 }
---
+++ src/components/other.data.js
@@ -0,0 +1,185 @@
+export default {
+  audioPath: './frere.mp3',
+  subtitle: '(Are You Sleeping)',
+  title: 'Frère Jacques',
+  paragraphs: [
+    {
+      header: {
+        imagePath: './frere.png',
+      },
+      sentences: [
+        {
+          lects: [
+            {
+              text: 'Estoy',
+            },
+            {
+              text: 'preocupada',
+            },
+            {
+              text: '.',
+            },
+          ],
+          translation: 'I am worried.',
+        },
+        {
+          lects: [
+            {
+              text: 'Estoy',
+            },
+            {
+              text: 'asustada',
+            },
+            {
+              text: '.',
+            },
+          ],
+          translation: 'I am scared.',
+        },
+        {
+          lects: [
+            {
+              text: 'Muy',
+            },
+            {
+              text: ","
+            },
+            {
+              text: 'muy',
+            },
+            {
+              text: ","
+            },
+            {
+              text: "asustada"
+            },
+            {
+              text: '.',
+            },
+          ],
+          translation: 'Very, very scared.',
+        },
+        {
+          lects: [
+            {
+              text: 'Hoy',
+            },
+            {
+              text: ","
+            },
+            {
+              text: 'tal vez',
+            },
+            {
+              text: ","
+            },
+            {
+              text: "voy a morir"
+            },
+            {
+              text: '.',
+            },
+          ],
+          translation: 'Today, maybe, I am going to die.',
+        },
+      ],
+    },
+    {
+      sentences: [
+        {
+          lects: [
+            {
+              text: 'Me gusto',
+            },
+            {
+              text: 'helado',
+            },
+            {
+              text: '.',
+            },
+          ],
+          translation: 'I like ice cream.',
+        },
+      ],
+    },
+    {
+      sentences: [
+        {
+          lects: [
+            {
+              text: 'Mi',
+            },
+            {
+              text: 'hermano',
+            },
+            {
+              text: 'siempre',
+            },
+            {
+              text: 'dice',
+            },
+            {
+              text: 'que',
+            },
+            {
+              text: 'es',
+            },
+            {
+              text: 'muy',
+            },
+            {
+              text: 'divertido',
+            },
+            {
+              text: '.',
+            },
+          ],
+          translation: 'My brother always says that it\'s very fun.',
+        },
+        {
+          lects: [
+            {
+              text: 'En',
+            },
+            {
+              text: 'este',
+            },
+            {
+              text: 'momento',
+            },
+            {
+              text: ',',
+            },
+            {
+              text: 'no se ve divertido',
+            },
+            {
+              text: '.',
+            },
+          ],
+          translation: 'Right now, it doesn\'t look like fun.',
+        },
+        {
+          lects: [
+            {
+              text: 'Parace',
+            },
+            {
+              text: 'una',
+            },
+            {
+              text: 'mala',
+            },
+            {
+              text: 'idea',
+            },
+            {
+              text: '.',
+            },
+          ],
+          translation: 'It seems like a bad idea.',
+        },
+      ],
+    },
+  ],
+}
