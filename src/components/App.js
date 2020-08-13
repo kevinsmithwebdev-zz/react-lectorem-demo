98504794b0a403473daadafc6370bf292b291a92
Kevin Smith <kevin.smith@jbhunt.com>
2020-08-13 18:52:59 -0500

    initial

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,6 @@ import Lectorem from 'react-lectorem';
 import storyData from './frere.data';
 import './App.css';
 
-require('react-dom');
-window.React2 = require('react');
-console.log('asdf react is equal', window.React1 === window.React2);
-
 const App = () => {
   const [shouldShowTranslation, setShouldShowTranslation] = useState(true);
   const [isTranslationBelow, setIsTranslationBelow] = useState(true);
@@ -14,19 +10,24 @@ const App = () => {
 
   const toggleValue = (flag, setFlag) =>  setFlag(!flag);
 
-  console.log('asdf1', shouldShowTranslation);
   return (
     <div className='App'>
-      <div onChange={ () => toggleValue(shouldShowTranslation, setShouldShowTranslation)}>
-        <input type='checkbox' value name='config' checked={ shouldShowTranslation }/>Translation
-      </div>
 
-      <div onChange={ () => toggleValue(isTranslationBelow, setIsTranslationBelow)}>
-        <input type='checkbox' value name='config' disabled={ !shouldShowTranslation } checked={ isTranslationBelow }/>Below
-      </div>
+      <div className='demoBox'>
+        <div className='controls'>
+          <button className='control' onClick={ () => toggleValue(isBySentence, setIsBySentence) }>
+            { isBySentence ? 'Organize by Paragraph' : 'Organize by Sentence' }
+          </button>
 
-      <div onChange={ () => toggleValue(isBySentence, setIsBySentence)}>
-        <input type='checkbox' value name='config' disabled={ !shouldShowTranslation } checked={ isBySentence }/>Sentence
+          <button className='control' onClick={ () => toggleValue(shouldShowTranslation, setShouldShowTranslation)}>
+            { shouldShowTranslation ? 'Hide Translation' : 'ShowTranslation' }
+          </button>
+
+          <button className={`control ${!shouldShowTranslation ? 'disabled' : '' }`} onClick={ () => toggleValue(isTranslationBelow, setIsTranslationBelow)}>
+            { isTranslationBelow ? 'Translation Beside' : 'Translation Below' }
+          </button>
+        </div>
+        <p className='controlText'>This is not part of the component - it's to show that these can be controlled.</p>
       </div>
 
       <Lectorem
--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
-window.React1 = require('react');
 
 ReactDOM.render(
   <React.StrictMode>
