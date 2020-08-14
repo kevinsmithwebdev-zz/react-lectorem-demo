9b9571b225db309f6e6bdf90f44318b821b36a62
Kevin Smith <kevin.smith@jbhunt.com>
2020-08-13 19:37:47 -0500

    Updates

---
+++ react-lectorem/precache-manifest.4cb3a498a702ef9ce5283a8f89387b4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04abbf485d2f901e4ffae44ed367701c",
+    "url": "/react-lectorem/index.html"
+  },
+  {
+    "revision": "c0e425b52710e270999f",
+    "url": "/react-lectorem/static/css/main.72063a77.chunk.css"
+  },
+  {
+    "revision": "1c8edf0b02e2a197fd76",
+    "url": "/react-lectorem/static/js/2.1ac960b9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-lectorem/static/js/2.1ac960b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0e425b52710e270999f",
+    "url": "/react-lectorem/static/js/main.d309fa5c.chunk.js"
+  },
+  {
+    "revision": "1487e432c1fe509beb54",
+    "url": "/react-lectorem/static/js/runtime-main.4530f3c5.js"
+  }
+]);
